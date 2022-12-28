11bbd3054993aca3ea84b5825ffacb0d5702f626
Leonardo Vargas <leu1607@hotmail.com>
2022-12-27 21:23:31 -0300

    feat: adicionando emote na mesagem do overlay

--- js/tmi.js
+++ js/tmi.js
@@ -4,7 +4,7 @@ const client = new tmi.Client({
 		username: 'leo__bot',
 		password: 'oauth:vdupbuqjacazzbz3zcfehkta5y68kb'
 	},
-	channels: [ 'leovargasdev' ]
+	channels: ['leovargasdev']
 });
 
 client.connect().catch(console.error);
@@ -30,13 +30,53 @@ function setMessageInParagraph(message, paragraph) {
 	});
 }
 
+function setMessageWithEmoteInParagraph(message, emotes, paragraph) {
+  const stringReplacements = [];
+
+  // iterate of emotes to access ids and positions
+  Object.entries(emotes).forEach(([id, positions]) => {
+    // use only the first position to find out the emote key word
+    const position = positions[0];
+    const [start, end] = position.split("-");
+    const stringToReplace = message.substring(
+      parseInt(start, 10),
+      parseInt(end, 10) + 1
+    );
+
+    stringReplacements.push({
+      stringToReplace: stringToReplace,
+      replacement: `<img src="https://static-cdn.jtvnw.net/emoticons/v1/${id}/1.0" />`,
+    });
+  });
+
+	// generate HTML and replace all emote keywords with image elements
+  const messageHTML = stringReplacements.reduce(
+    (acc, { stringToReplace, replacement }) => {
+      // obs browser doesn't seam to know about replaceAll
+      return acc.split(stringToReplace).join(replacement);
+    },
+    message
+  );
+
+	const paragraphMessage = document.createElement('p')
+	paragraphMessage.innerHTML = messageHTML
+
+	paragraph.appendChild(paragraphMessage)
+}
+
 client.on('message', (channel, tags, message, self) => {
   const paragraph = document.createElement('p')
 	paragraph.setAttribute('id', tags.id)
 
 	setUsernameInParagraph(tags, paragraph)
 
-	setMessageInParagraph(message, paragraph)
+	const isEmotes = tags.emotes
+
+	if(isEmotes) {
+		setMessageWithEmoteInParagraph(message, tags.emotes, paragraph)
+	} else {
+		setMessageInParagraph(message, paragraph)
+	}
 
   chat.append(paragraph)
 
@@ -46,7 +86,7 @@ client.on('message', (channel, tags, message, self) => {
     easing: 'easeOutElastic',
     opacity: [0, 1],
     delay: (t, i) => i * 40,
-		translateX: [-300, 0]
+		translateY: [-300, 0]
   })
 
 	paragraph.scrollIntoView({block: 'end', behavior: 'smooth'})
